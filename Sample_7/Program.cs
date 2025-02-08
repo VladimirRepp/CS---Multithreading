8d5437a647aeb019c4e63ec125c819fc47d6e23c
VladimirRepp <57412538+VladimirRepp@users.noreply.github.com>
2025-02-08 15:47:13 +0700

    Add files via upload

---
+++ Sample_7/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading;
+
+// Пример создания и запуска потоков
+namespace Sample_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sample0();
+            Sample1();
+        }
+
+        private static void Print_ForSample0() => Console.WriteLine("Hello Threads!");
+
+        private static void Sample0()
+        {
+            // Инициализируем потоки 
+            Thread myThread = new Thread(Print_ForSample0);
+            Thread myThread1 = new Thread(new ThreadStart(Print_ForSample0));
+            Thread myThread2 = new Thread(() => Console.WriteLine("Hello Threads!"));
+
+            // Запустить потоки
+            myThread.Start();
+            myThread1.Start();
+            myThread2.Start();
+
+            Console.ReadLine();
+        }
+
+        private static void Sample1()
+        {
+            Thread thread = new Thread(Print_ForSample1);
+            thread.Start();
+            thread.Priority = ThreadPriority.Normal;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Вывод ГЛАВНОГО потока №{i}");
+                Thread.Sleep(1000);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void Print_ForSample1() 
+        { 
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Вывод второго потока №{i}");
+                Thread.Sleep(400);
+            }
+        }
+    }
+}
